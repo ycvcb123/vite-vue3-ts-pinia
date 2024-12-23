0b48e53d3c37a510d718e6b3f0e36e96c13374e5
yezw <yangyang1991318@qq.com>
2024-12-23 14:44:15 +0800

    pinia router vueuse 工具库

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import router from './router';
 
-createApp(App).mount('#app')
+// 创建vue实例
+const app = createApp(App);
+// 挂载pinia
+app.use(store);
+// 挂在路由
+app.use(router);
+// 挂载实例
+app.mount('#app');
